30c401dd5f17775d62471baff0a7d27fc696d10b
master <master@gmail.com>
2023-04-19 12:09:44 +0800

    load cr5 and control with Moveit!

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/hisen/Project/ROS/CR_Mine/src
+# Build directory: /home/hisen/Project/ROS/CR_Mine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("cr5_moveit")
+subdirs("dobot_gazebo")
---
+++ build/cr5_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hisen/Project/ROS/CR_Mine/src/cr5_moveit
+# Build directory: /home/hisen/Project/ROS/CR_Mine/build/cr5_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
